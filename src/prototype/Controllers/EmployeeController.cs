913f85ce979c58f7ba993fc3c098caf00a3ecd03
Mike Haas <mike.haas@raymondjames.com>
2016-11-23 13:42:26 -0500

    Initial commit

---
+++ src/prototype/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace prototype
+{
+	[Route("api/[controller]")]
+	public class EmployeeController : Controller
+	{
+		public IActionResult Get()
+		{
+			return Json("okie dokie");
+		}
+	}
+}
