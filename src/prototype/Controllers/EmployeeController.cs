d0a2c7773d35aa195dff84efb90714ae99ea05fe
Mike Haas <mike.haas@raymondjames.com>
2016-11-23 18:11:40 -0500

    Ignore vscode workspace files

--- src/prototype/Controllers/EmployeeController.cs
+++ src/prototype/Controllers/EmployeeController.cs
@@ -1,14 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿// using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace prototype
 {
-	[Route("api/[controller]")]
-	public class EmployeeController : Controller
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
 	{
-		public IActionResult Get()
-		{
-			return Json("okie dokie");
-		}
-	}
+        public IActionResult Get()
+        {
+            var message = "Hello World!";
+            return Json(message);
+        }
+    }
 }
